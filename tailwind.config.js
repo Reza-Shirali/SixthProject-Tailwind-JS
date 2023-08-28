41853c9e9462552501ef9cfef81633321e81a369
Reza Shirali <shir.ez.aliofficial@gmail.com>
2023-08-28 13:17:46 +0330

    init project

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,18 @@ module.exports = {
   theme: {
     extend: {},
     screens: {
-      'sm': '640px',
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
-    }
+    },
   },
   plugins: [],
-}
-
+};
